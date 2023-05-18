9fe8741114beb95be6dde3c6376d50f10ac91895
robertschelling <robert.schelling@mllrdev.com>
2023-05-18 18:25:37 +0800

    testing vue components

---
+++ src/components/Graph/BaseGraph.vue
@@ -0,0 +1,19 @@
+<template>
+  <center>
+    <BaseNode v-for="item in list" :key="item" :message="item.toString()"> </BaseNode>
+  </center>
+  <button
+    @click="
+      () => {
+        list[2]++
+      }
+    "
+  ></button>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import BaseNode from '../Node/BaseNode.vue'
+
+const list = ref(new Array(10).fill(1))
+</script>
---
+++ src/components/Node/BaseNode.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="square">{{ message }}</button>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import { onBeforeUpdate } from 'vue'
+
+defineProps({ message: String })
+
+onMounted(() => {
+  console.log('RENDER NODE')
+})
+</script>
+
+<style>
+@import './styles.scss';
+</style>
